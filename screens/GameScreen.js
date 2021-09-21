00fe208c7cff88a38c0f860793e6d86430d45b20
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-09-21 14:26:30 +0800

    Created platform specific file name extensions.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -9,6 +9,7 @@ import {
   Dimensions
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { ScreenOrientation } from 'expo';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -35,6 +36,8 @@ const renderListItem = (listLength, itemData) => (
 );
 
 const GameScreen = props => {
+  // ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
